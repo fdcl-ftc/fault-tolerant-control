01a23190203ed629643fc40adf971fd97b198cee
Jinrae Kim <kjl950403@gmail.com>
2021-04-28 12:10:58 +0900

    remove deprecated lines
    Co-authored-by: Seong-hun Kim <kshoon92@gmail.com>

--- faults/faults.py
+++ faults/faults.py
@@ -64,31 +64,6 @@ class Float(LoE):
         return super().get(t, u)
 
 
-## Deprecated
-# """
-#     HexacopterFault
-
-# `self.loe_func` is a function of the effectiveness of each rotor w.r.t. time.
-# """
-# class HexacopterFault:
-#     def __init__(self, func):
-#         self.effectiveness_func = func
-
-#     def get(self, t):
-#         _effectiveness = self.effectiveness_func(t)
-#         if self.is_valid(_effectiveness):
-#             effectiveness = _effectiveness
-#         else:
-#             raise ValueError("Invalid effectiveness")
-#         return effectiveness
-
-#     def is_valid(self, effectiveness):
-#         valid = True
-#         if effectiveness.shape != (6, 6):
-#             valid = False
-#         elif not all(eff >= 0.0 for eff in effectiveness.flatten()):
-#             valid = False
-#         return valid
 
 
 if __name__ == "__main__":
