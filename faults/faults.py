e8fa14f5acf7af85b791fb42737afc14f748bc04
Jinrae Kim <kjl950403@gmail.com>
2021-04-28 12:10:45 +0900

    remove deprecated lines
    Co-authored-by: Seong-hun Kim <kshoon92@gmail.com>

--- faults/faults.py
+++ faults/faults.py
@@ -108,24 +108,3 @@ if __name__ == "__main__":
     ]
     for fault in faults:
         test(fault)
-
-    ## Deprecated
-    # effectiveness_default = np.diag(np.ones(6))
-    # effectiveness_fault = np.diag(np.array([0.5, 1.0, 1.0, 1.0, 1.0, 1.0]))
-    # def effectiveness_func(t):
-    #     if t < 1:
-    #         effectiveness = effectiveness_default
-    #     else:
-    #         effectiveness = effectiveness_fault
-    #     return effectiveness
-    # fault = HexacopterFault(effectiveness_func)
-    # t = 0.0
-    # if any(fault.get(t).flatten() != effectiveness_default.flatten()):
-    #     raise ValueError("Something went wrong")
-    # else:
-    #     print("Test passed")
-    # t = 10.0
-    # if any(fault.get(t).flatten() != effectiveness_fault.flatten()):
-    #     raise ValueError("Something went wrong")
-    # else:
-    #     print("Test passed")
