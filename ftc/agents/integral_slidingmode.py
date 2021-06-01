dc6116ddf89e2cce3fd030683b9f2bc1fb496014
miaerosae <sea9712@snu.ac.kr>
2021-06-01 17:28:24 +0900

    Integral SMC

---
+++ ftc/agents/integral_slidingmode.py
@@ -0,0 +1,153 @@
+import numpy as np
+from numpy import cos
+
+from fym.core import BaseEnv, BaseSystem
+from fym.utils.rot import quat2angle
+
+
+def sat(s, eps):
+    if s > eps:
+        return 1
+    elif s < -eps:
+        return -1
+    else:
+        return s/eps
+
+
+class IntegralSMC(BaseEnv):
+    def __init__(self, env, ic, ref0):
+        super().__init__()
+        self.env = env
+        self.ic_ = np.vstack((ic[0:6], np.vstack(quat2angle(ic[6:10])[::-1]), ic[10:]))
+        self.ref0_ = np.vstack((ref0[0:6], np.vstack(quat2angle(ref0[6:10])[::-1]), ref0[10:]))
+        self.d, self.m, self.g, self.J = env.plant.d, env.plant.m, env.plant.g, env.plant.J
+        self.p1 = BaseSystem(0)
+        self.p2 = BaseSystem(0)
+        self.p3 = BaseSystem(0)
+        self.p4 = BaseSystem(0)
+
+    def get_ddot(self):
+        obs = self.obs
+        action = self.action
+        J = self.J
+        m, g, d = self.m, self.g, self.d
+        Ixx = J[0, 0]
+        Iyy = J[1, 1]
+        Izz = J[2, 2]
+
+        obs = np.vstack((obs))
+        obs_ = np.vstack((obs[0:6], np.vstack(quat2angle(obs[6:10])[::-1]), obs[10:]))
+        phi, theta, psi, phid, thetad, psid = obs_[6:]
+        F, M1, M2, M3 = action
+
+        zdd = g - cos(phi)*cos(theta)*F/m
+        phidd = (Iyy-Izz)/Ixx*thetad*psid + M1/Ixx*d
+        thetadd = (Izz-Ixx)/Iyy*phid*psid + M2/Iyy*d
+        psidd = (Ixx-Iyy)/Izz*phid*thetad + M3/Izz*d
+
+        return zdd, phidd, thetadd, psidd
+
+    def deriv(self):
+        K = self.K
+        Kc = self.Kc
+
+        K1, K2, K3, K4 = K
+        k11, k12 = K1
+        k21, k22 = K2
+        k31, k32 = K3
+        k41, k42 = K4
+
+        kc1, kc2, kc3, kc4 = Kc
+
+        dp1 = -self.e_zdd + k12*self.e_zd + k11*self.e_z
+        dp2 = -self.e_phidd + k22*self.e_phid + k21*self.e_phi
+        dp3 = -self.e_thetadd + k32*self.e_thetad + k31*self.e_theta
+        dp4 = -self.e_psidd + k42*self.e_psid + k41*self.e_psi
+
+        return dp1, dp2, dp3, dp4
+
+    def set_dot(self):
+        dots = self.deriv()
+        self.p1.dot, self.p2.dot, self.p3.dot, self.p4.dot = dots
+
+    def get_action(self, obs, action, ref, K, Kc, PHI, t):
+        self.obs = obs
+        self.action = action
+        self.ref = ref
+        self.K = K
+        self.Kc = Kc
+        K1, K2, K3, K4 = K
+        k11, k12 = K1
+        k21, k22 = K2
+        k31, k32 = K3
+        k41, k42 = K4
+        kc1, kc2, kc3, kc4 = Kc
+        PHI1, PHI2, PHI3, PHI4 = PHI
+        # model
+        J = self.J
+        Ixx = J[0, 0]
+        Iyy = J[1, 1]
+        Izz = J[2, 2]
+        m, g, d = self.m, self.g, self.d
+        # observation
+        obs = np.vstack((obs))
+        obs_ = np.vstack((obs[0:6], np.vstack(quat2angle(obs[6:10])[::-1]), obs[10:]))
+        z, zd = obs_[2], obs_[5]
+        phi, theta, psi, phid, thetad, psid = obs_[6:]
+        zdd, phidd, thetadd, psidd = self.get_ddot()
+        # reference
+        ref_ = np.vstack((ref[0:6], np.vstack(quat2angle(ref[6:10])[::-1]), ref[10:]))
+        z_r, zd_r = ref_[2], ref_[5]
+        phi_r, theta_r, psi_r, phid_r, thetad_r, psid_r = ref_[6:]
+        zdd_r = 0
+        phidd_r = 0
+        thetadd_r = 0
+        psidd_r = 0
+        # initial condition
+        z0, z0d = self.ic_[2], self.ic_[5]
+        phi0, theta0, psi0, phi0d, theta0d, psi0d = self.ic_[6:]
+        z0_r, z0d_r = self.ref0_[2], self.ref0_[5]
+        phi0_r, theta0_r, psi0_r, phi0d_r, theta0d_r, psi0d_r = self.ref0_[6:]
+        # error definition
+        self.e_z = z - z_r
+        self.e_zd = zd - zd_r
+        self.e_zdd = zdd - zdd_r
+        self.e_phi = phi - phi_r
+        self.e_phid = phid - phid_r
+        self.e_phidd = phidd - phidd_r
+        self.e_theta = theta - theta_r
+        self.e_thetad = thetad - thetad_r
+        self.e_thetadd = thetadd - thetadd_r
+        self.e_psi = psi - psi_r
+        self.e_psid = psid - psid_r
+        self.e_psidd = psidd - psidd_r
+        # h**(-1) function definition
+        h1 = -m/cos(phi)/cos(theta)
+        h2 = Ixx/d
+        h3 = Iyy/d
+        h4 = Izz/d
+        # sliding surface
+        s1 = self.e_z + self.e_zd - (z0-z0_r) - (z0d-z0d_r) + self.p1.state
+        s2 = self.e_phi + self.e_phid - (phi0-phi0_r) - (phi0d-phi0d_r) + self.p2.state
+        s3 = self.e_theta + self.e_thetad - (theta0-theta0_r) - (theta0d-theta0d_r) + self.p3.state
+        s4 = self.e_psi + self.e_psid - (psi0-psi0_r) - (psi0d-psi0d_r) + self.p4.state
+        # get FM
+        F = h1*(zdd_r - k12*self.e_zd - k11*self.e_z - g) - h1*kc1*sat(s1, PHI1)
+        M1 = h2*(phidd_r - k22*self.e_phid - k21*self.e_phi - (Iyy-Izz)/Ixx*thetad*psid) - h2*kc2*sat(s2, PHI2)
+        M2 = h3*(thetadd_r - k32*self.e_thetad - k31*self.e_theta - (Izz-Ixx)/Iyy*phid*psid) - h3*kc3*sat(s3, PHI3)
+        M3 = h4*(psidd_r - k42*self.e_psid - k41*self.e_psi - (Ixx-Iyy)/Izz*phid*thetad) - h4*kc4*sat(s4, PHI4)
+
+        if F > m*g*5:
+            F = m*g*5
+        elif F < 0:
+            F = 0
+        else:
+            F = F
+
+        action = np.vstack((F, M1, M2, M3))
+
+        return action
+
+
+if __name__ == "__main__":
+    pass
--- test/IntegralSMC.py
+++ test/IntegralSMC.py
@@ -6,7 +6,7 @@ import fym.logging
 from fym.core import BaseEnv, BaseSystem
 
 from ftc.models.multicopter import Multicopter
-from ftc.agent.integral_slidingmode import IntegralSMC
+from ftc.agents.integral_slidingmode import IntegralSMC
 
 
 class Env(BaseEnv):
@@ -15,7 +15,6 @@ class Env(BaseEnv):
                  vel=np.zeros((3, 1)),
                  quat=np.vstack((1, 0, 0, 0)),
                  omega=np.zeros((3, 1)),
-                 ref=np.vstack([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]),
                  rtype="hexa-x"):
         super().__init__(dt=0.001, max_t=10)
         self.plant = Multicopter(pos, vel, quat, omega, rtype)
@@ -44,11 +43,11 @@ class Env(BaseEnv):
         self.plant.set_dot(t, rotors)
 
 
-def run(env, pos, quat, ref, dtype, agent=None):
+def run(env, pos, quat, ref, K, Kc, PHI, agent=None):
     obs = env.reset()
     logger = fym.logging.Logger(path='data.h5')
-    gamma_tune = np.array([1, 1, 1, 1])
-    kd_tune = np.array([25, 1, 1, ])
+    action = np.zeros((4, 1))
+    t = 0
 
     while True:
         env.render()
@@ -56,7 +55,7 @@ def run(env, pos, quat, ref, dtype, agent=None):
         if agent is None:
             action = 0
         else:
-            action = agent.get_action(obs, ref, gamma_tune, kd_tune, dtype=dtype)
+            action = agent.get_action(obs, action, ref, K, Kc, PHI, t)
 
         t, next_obs, info, rotors, done = env.step(action)
         obs = next_obs
@@ -69,9 +68,99 @@ def run(env, pos, quat, ref, dtype, agent=None):
     logger.close()
 
 
-def test_integralSMC(pos, quat, ref, dtype):
-    env = Env(pos, quat)
-    agent = IntegralSMC(env, ref)
-    run(env, pos, quat, ref, agent, dtype)
+def test_integralSMC(pos, quat, ic, ref, ref0, K, Kc, PHI):
+    env = Env(pos=pos, quat=quat)
+    agent = IntegralSMC(env, ic, ref0)
+    run(env, pos, quat, ref, K, Kc, PHI, agent)
     plot_var()
     plt.show()
+
+
+def plot_var():
+    data = fym.logging.load('data.h5')
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(4, 1, 1)
+    ax2 = fig.add_subplot(4, 1, 2, sharex=ax1)
+    ax3 = fig.add_subplot(4, 1, 3, sharex=ax1)
+    ax4 = fig.add_subplot(4, 1, 4, sharex=ax1)
+
+    ax1.plot(data['t'], data['plant']['pos'].squeeze())
+    ax2.plot(data['t'], data['plant']['vel'].squeeze())
+    ax3.plot(data['t'], data['plant']['quat'].squeeze())
+    ax4.plot(data['t'], data['plant']['omega'].squeeze())
+
+    ax1.set_ylabel('Position')
+    ax1.legend([r'$x$', r'$y$', r'$z$'])
+    ax1.grid(True)
+
+    ax2.set_ylabel('Velocity')
+    ax2.legend([r'$u$', r'$v$', r'$w$'])
+    ax2.grid(True)
+
+    ax3.set_ylabel('Quaternion')
+    ax3.legend([r'$q_0$', r'$q_1$', r'$q_2$', r'$q_3$'])
+    ax3.grid(True)
+
+    ax4.set_ylabel('Angular Velocity')
+    ax4.legend([r'$p$', r'$q$', r'$r$'])
+    ax4.set_xlabel('Time [sec]')
+    ax4.grid(True)
+
+    plt.tight_layout()
+
+    fig2 = plt.figure()
+    ax1 = fig2.add_subplot(6, 1, 1)
+    ax2 = fig2.add_subplot(6, 1, 2, sharex=ax1)
+    ax3 = fig2.add_subplot(6, 1, 3, sharex=ax1)
+    ax4 = fig2.add_subplot(6, 1, 4, sharex=ax1)
+    ax5 = fig2.add_subplot(6, 1, 5, sharex=ax1)
+    ax6 = fig2.add_subplot(6, 1, 6, sharex=ax1)
+
+    ax1.plot(data['t'], data['rotors'].squeeze()[:, 0])
+    ax2.plot(data['t'], data['rotors'].squeeze()[:, 1])
+    ax3.plot(data['t'], data['rotors'].squeeze()[:, 2])
+    ax4.plot(data['t'], data['rotors'].squeeze()[:, 3])
+    ax5.plot(data['t'], data['rotors'].squeeze()[:, 4])
+    ax6.plot(data['t'], data['rotors'].squeeze()[:, 5])
+
+    ax1.set_ylabel('rotor1')
+    ax1.grid(True)
+    ax2.set_ylabel('rotor2')
+    ax2.grid(True)
+    ax3.set_ylabel('rotor3')
+    ax3.grid(True)
+    ax4.set_ylabel('rotor4')
+    ax4.grid(True)
+    ax5.set_ylabel('rotor5')
+    ax5.grid(True)
+    ax6.set_ylabel('rotor6')
+    ax6.grid(True)
+    ax6.set_xlabel('Time [sec]')
+
+    plt.tight_layout()
+
+
+if __name__ == "__main__":
+    # reference
+    rpos = np.vstack((0, 0, -10))
+    rvel = np.zeros((3, 1))
+    rquat = np.vstack((1, 0, 0, 0))
+    romega = np.zeros((3, 1))
+    ref = np.vstack((rpos, rvel, rquat, romega))
+    ref0 = np.vstack((rpos, rvel, rquat, romega))
+    # perturbation
+    pos_pertb = np.vstack([0, 0, -0])
+    yaw = 0
+    pitch = 5
+    roll = 0
+    quat_pertb = angle2quat(*np.deg2rad([yaw, pitch, roll])[::-1])
+    ic = np.vstack((pos_pertb, np.zeros((3, 1)), quat_pertb, np.zeros((3, 1))))
+    # gain
+    K = np.array([[25, 10],
+                  [1000, 20],
+                  [1000, 20],
+                  [25, 10]])
+    Kc = np.vstack((5, 10, 10, 5))
+    PHI = np.vstack([1.2] * 4)
+    test_integralSMC(pos_pertb, quat_pertb, ic, ref, ref0, K, Kc, PHI)
