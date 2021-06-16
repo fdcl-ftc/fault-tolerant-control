affa25d9901ef54d167522965147e49567c531a8
Jinrae Kim <kjl950403@gmail.com>
2021-06-16 18:34:31 +0900

    Update ftc/evaluate/evaluate.py
    Co-authored-by: Seong-hun Kim <kshoon92@gmail.com>

--- ftc/evaluate/evaluate.py
+++ ftc/evaluate/evaluate.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def calculate_recovery_rate(errors: np.ndarray, threshold: float=0.5):
     assert threshold > 0
-    recovery_rate = np.average(errors <= threshold)
+    recovery_rate = np.average(np.abs(errors) <= threshold)
     return recovery_rate
 
 
