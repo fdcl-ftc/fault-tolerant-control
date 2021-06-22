9929f1756e27f5ce43223eafe3144decb47535c9
Seong-hun Kim <kshoon92@gmail.com>
2021-06-22 20:23:33 +0900

    Fix glitches

--- ftc/agents/fdi.py
+++ ftc/agents/fdi.py
@@ -1,17 +1,27 @@
 import numpy as np
 from numpy import searchsorted as ss
+from functools import reduce
+
 from fym.core import BaseEnv, BaseSystem
+
+
+def get_loe(actuator_faults, no_act):
+    actuator_faults = sorted(actuator_faults, key=lambda x: x.time)
+    loe = np.eye(no_act)
+    yield loe
+    for act_fault in actuator_faults:
+        loe = loe.copy()
+        loe[act_fault.index, act_fault.index] = act_fault.level
+        yield loe
 
 
 class SimpleFDI():
     def __init__(self, actuator_faults, no_act, delay=0.):
         self.delay = delay
-        self.loe = [
-            np.eye(no_act),
-            *map(lambda x: x.level * np.eye(no_act),
-                 sorted(actuator_faults, key=lambda x: x.time))
-        ]
-        self.fault_times = np.array([0] + [x.time for x in actuator_faults])
+
+        self.loe = list(get_loe(actuator_faults, no_act))
+        self.fault_times = np.array(
+            [0] + sorted([x.time for x in actuator_faults]))
 
     def get(self, t):
         index = max(ss(self.fault_times, t - self.delay, side="right") - 1, 0)
@@ -20,3 +30,14 @@ class SimpleFDI():
     def get_real(self, t):
         index = ss(self.fault_times, t, side="right") - 1
         return self.loe[index]
+
+
+if __name__ == "__main__":
+    from ftc.faults.actuator import LoE, LiP, Float
+
+    actuator_faults = [
+        LoE(time=3, index=0, level=0.7),
+        LoE(time=6, index=2, level=0.8),
+        LoE(time=1, index=0, level=0.6),
+    ]
+    fdi = SimpleFDI(actuator_faults, 6)
