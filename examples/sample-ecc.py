b58d3c04c2e1371b09432b860a781ae7e85c70f0
Hanna Lee <dlgkssk707@gmail.com>
2022-10-19 16:28:32 +0900

    wip

--- examples/sample-ecc.py
+++ examples/sample-ecc.py
@@ -93,37 +93,37 @@ class Env(fym.BaseEnv):
 
 def parsim(N=1, seed=0):
     np.random.seed(seed)
-    # pos = np.random.uniform(0, 0, size=(N, 3, 1))
-    # vel = np.random.uniform(0, 0, size=(N, 3, 1))
-    # angle = np.random.uniform(*np.deg2rad((-10, 10)), size=(N, 3, 1))
-    # omega = np.random.uniform(*np.deg2rad((-0, 0)), size=(N, 3, 1))
-    # k1v = np.random.uniform(5, 20, size=(N, 1))
-    # k1a = np.random.uniform(10, 50, size=(N, 2))
-    # k1a_psi = np.random.uniform(0.1, 5, size=(N, 1))
-    # k2v = np.random.uniform(5, 20, size=(N, 1))
-    # k2a = np.random.uniform(10, 50, size=(N, 2))
-    # k2a_psi = np.random.uniform(0.1, 5, size=(N, 1))
-    # k1 = np.hstack((k1v, k1a, k1a_psi))
-    # k2 = np.hstack((k2v, k2a, k2a_psi))
+    pos = np.random.uniform(0, 0, size=(N, 3, 1))
+    vel = np.random.uniform(1, 1, size=(N, 3, 1))
+    angle = np.random.uniform(*np.deg2rad((-10, 10)), size=(N, 3, 1))
+    omega = np.random.uniform(*np.deg2rad((-10, 10)), size=(N, 3, 1))
+    k1v = np.random.uniform(5, 20, size=(N, 1))
+    k1a = np.random.uniform(10, 50, size=(N, 2))
+    k1a_psi = np.random.uniform(0.1, 5, size=(N, 1))
+    k2v = np.random.uniform(5, 20, size=(N, 1))
+    k2a = np.random.uniform(10, 50, size=(N, 2))
+    k2a_psi = np.random.uniform(0.1, 5, size=(N, 1))
+    k1 = np.hstack((k1v, k1a, k1a_psi))
+    k2 = np.hstack((k2v, k2a, k2a_psi))
 
-    test_result = torch.load("test_result.pt")
-    dataset = test_result["dataset"]
-    initial_state = dataset["condition"]  # d x n
-    gain = dataset["decision"]  # d x m
-    predicted_optimal_gain = dataset["predicted_optimal_decision"]  # d x m
+    # test_result = torch.load("test_result.pt")
+    # dataset = test_result["dataset"]
+    # initial_state = dataset["condition"]  # d x n
+    # gain = dataset["decision"]  # d x m
+    # predicted_optimal_gain = dataset["predicted_optimal_decision"]  # d x m
 
-    pos = np.zeros((1000, 3, 1))
-    vel = np.zeros((1000, 3, 1))
-    angle = np.zeros((1000, 3, 1))
-    omega = np.zeros((1000, 3, 1))
-    pos[:, 2, 0] = initial_state[:, 0]
-    vel[:, 2, 0] = initial_state[:, 1]
-    angle[:, :, 0] = initial_state[:, 2:5]
-    omega[:, :, 0] = initial_state[:, 5:8]
-    # k1 = gain[:, :4]
-    # k2 = gain[:, 4:]
-    k1 = predicted_optimal_gain[:, :4]
-    k2 = predicted_optimal_gain[:, 4:]
+    # pos = np.zeros((1000, 3, 1))
+    # vel = np.zeros((1000, 3, 1))
+    # angle = np.zeros((1000, 3, 1))
+    # omega = np.zeros((1000, 3, 1))
+    # pos[:, 2, 0] = initial_state[:, 0]
+    # vel[:, 2, 0] = initial_state[:, 1]
+    # angle[:, :, 0] = initial_state[:, 2:5]
+    # omega[:, :, 0] = initial_state[:, 5:8]
+    # # k1 = gain[:, :4]
+    # # k2 = gain[:, 4:]
+    # k1 = predicted_optimal_gain[:, :4]
+    # k2 = predicted_optimal_gain[:, 4:]
 
     initials = np.stack((pos, vel, angle, omega), axis=1)
     gains = np.stack((k1, k2), axis=1)
@@ -135,7 +135,8 @@ if __name__ == "__main__":
     seed = 0
     parsim(N, seed)
 
-    # data = fym.load("data/env_0000.h5")["env"]
+    # dataopt = fym.load("dataopt/env_0010.h5")["env"]
+    # data = fym.load("data/env_0010.h5")["env"]
 
     # """ Figure 1 - States """
     # fig, axes = plt.subplots(3, 4, figsize=(18, 5), squeeze=False, sharex=True)
@@ -143,18 +144,21 @@ if __name__ == "__main__":
     # """ Column 1 - States: Position """
     # ax = axes[0, 0]
     # ax.plot(data["t"], data["plant"]["pos"][:, 0].squeeze(-1), "k-")
+    # ax.plot(dataopt["t"], dataopt["plant"]["pos"][:, 0].squeeze(-1), "b-.")
     # ax.plot(data["t"], data["posd"][:, 0].squeeze(-1), "r--")
     # ax.set_ylabel(r"$x$, m")
-    # ax.legend(["Response", "Command"], loc="upper right")
+    # ax.legend(["Res (Random)", "Res (PLSE)", "Command"], loc="upper right")
     # ax.set_xlim(data["t"][0], data["t"][-1])
 
     # ax = axes[1, 0]
     # ax.plot(data["t"], data["plant"]["pos"][:, 1].squeeze(-1), "k-")
+    # ax.plot(dataopt["t"], dataopt["plant"]["pos"][:, 1].squeeze(-1), "b-.")
     # ax.plot(data["t"], data["posd"][:, 1].squeeze(-1), "r--")
     # ax.set_ylabel(r"$y$, m")
 
     # ax = axes[2, 0]
     # ax.plot(data["t"], data["plant"]["pos"][:, 2].squeeze(-1), "k-")
+    # ax.plot(dataopt["t"], dataopt["plant"]["pos"][:, 2].squeeze(-1), "b-.")
     # ax.plot(data["t"], data["posd"][:, 2].squeeze(-1), "r--")
     # ax.set_ylabel(r"$z$, m")
 
@@ -163,14 +167,17 @@ if __name__ == "__main__":
     # """ Column 2 - States: Velocity """
     # ax = axes[0, 1]
     # ax.plot(data["t"], data["plant"]["vel"][:, 0].squeeze(-1), "k-")
+    # ax.plot(dataopt["t"], dataopt["plant"]["vel"][:, 0].squeeze(-1), "b-.")
     # ax.set_ylabel(r"$v_x$, m/s")
 
     # ax = axes[1, 1]
     # ax.plot(data["t"], data["plant"]["vel"][:, 1].squeeze(-1), "k-")
+    # ax.plot(dataopt["t"], dataopt["plant"]["vel"][:, 1].squeeze(-1), "b-.")
     # ax.set_ylabel(r"$v_y$, m/s")
 
     # ax = axes[2, 1]
     # ax.plot(data["t"], data["plant"]["vel"][:, 2].squeeze(-1), "k-")
+    # ax.plot(dataopt["t"], dataopt["plant"]["vel"][:, 2].squeeze(-1), "b-.")
     # ax.set_ylabel(r"$v_z$, m/s")
 
     # ax.set_xlabel("Time, sec")
@@ -178,16 +185,19 @@ if __name__ == "__main__":
     # """ Column 3 - States: Euler angles """
     # ax = axes[0, 2]
     # ax.plot(data["t"], np.rad2deg(data["ang"][:, 0].squeeze(-1)), "k-")
+    # ax.plot(dataopt["t"], np.rad2deg(dataopt["ang"][:, 0].squeeze(-1)), "b-.")
     # ax.plot(data["t"], np.rad2deg(data["angd"][:, 0].squeeze(-1)), "r--")
     # ax.set_ylabel(r"$\phi$, deg")
 
     # ax = axes[1, 2]
     # ax.plot(data["t"], np.rad2deg(data["ang"][:, 1].squeeze(-1)), "k-")
+    # ax.plot(dataopt["t"], np.rad2deg(dataopt["ang"][:, 1].squeeze(-1)), "b-.")
     # ax.plot(data["t"], np.rad2deg(data["angd"][:, 1].squeeze(-1)), "r--")
     # ax.set_ylabel(r"$\theta$, deg")
 
     # ax = axes[2, 2]
     # ax.plot(data["t"], np.rad2deg(data["ang"][:, 2].squeeze(-1)), "k-")
+    # ax.plot(dataopt["t"], np.rad2deg(dataopt["ang"][:, 2].squeeze(-1)), "b-.")
     # ax.plot(data["t"], np.rad2deg(data["angd"][:, 2].squeeze(-1)), "r--")
     # ax.set_ylabel(r"$\psi$, deg")
 
@@ -196,14 +206,17 @@ if __name__ == "__main__":
     # """ Column 4 - States: Angular rates """
     # ax = axes[0, 3]
     # ax.plot(data["t"], np.rad2deg(data["plant"]["omega"][:, 0].squeeze(-1)), "k-")
+    # ax.plot(dataopt["t"], np.rad2deg(dataopt["plant"]["omega"][:, 0].squeeze(-1)), "b-.")
     # ax.set_ylabel(r"$p$, deg/s")
 
     # ax = axes[1, 3]
     # ax.plot(data["t"], np.rad2deg(data["plant"]["omega"][:, 1].squeeze(-1)), "k-")
+    # ax.plot(dataopt["t"], np.rad2deg(dataopt["plant"]["omega"][:, 1].squeeze(-1)), "b-.")
     # ax.set_ylabel(r"$q$, deg/s")
 
     # ax = axes[2, 3]
     # ax.plot(data["t"], np.rad2deg(data["plant"]["omega"][:, 2].squeeze(-1)), "k-")
+    # ax.plot(dataopt["t"], np.rad2deg(dataopt["plant"]["omega"][:, 1].squeeze(-1)), "b-.")
     # ax.set_ylabel(r"$r$, deg/s")
 
     # ax.set_xlabel("Time, sec")
